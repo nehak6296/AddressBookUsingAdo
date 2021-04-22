a2740599749d86e0a59a696d3340524f2a0bf1b2
Neha <nehak6296@gmail.com>
2021-04-22 23:45:39 +0530

    [Neha]Added:Add Contacts to Address Book Server

---
+++ AddressBookSystem/RestSharpAddressBookTestProject/JsonContactsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpAddressBookTestProject
+{
+    class JsonContactsModel
+    {
+        public int id { get; set; }
+        public string AddressBookName { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone_number { get; set; }
+        public string Email { get; set; }
+    }
+}
--- AddressBookSystem/RestSharpAddressBookTestProject/UnitTest1.cs
+++ AddressBookSystem/RestSharpAddressBookTestProject/UnitTest1.cs
@@ -27,6 +27,7 @@ namespace RestSharpAddressBookTestProject
             IRestResponse response = client.Execute(request);
             return response;
         }
+        //UC22
         [TestMethod]
         public void onCallingGETApi_ReturnContactsList()
         {
@@ -41,6 +42,7 @@ namespace RestSharpAddressBookTestProject
                 System.Console.WriteLine( "First Name: " + item.First_name +"\t" +"Last Name: " + item.Last_name);
             }
         }
+        //UC23
         [TestMethod]
         public void givenContacts_OnPost_ShouldReturnAddedContacts()
         {
@@ -48,14 +50,28 @@ namespace RestSharpAddressBookTestProject
             JObject jObjectbody = new JObject();
             jObjectbody.Add("First_name", "Clark");
             jObjectbody.Add("Last_name", "Dsoza");
+            jObjectbody.Add("Address", "kj road");
+            jObjectbody.Add("City", "NY");
+            jObjectbody.Add("State", "bst");
+            jObjectbody.Add("Zip", "123445");
+            jObjectbody.Add("Phone_number", "9999979000");
+            jObjectbody.Add("Email", "cdd@gmail.com");
+            jObjectbody.Add("AddressBookName", "AddressBook1");
             request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
 
             //act
             IRestResponse response = client.Execute(request);
-            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
-            ContactsModel dataResponse = JsonConvert.DeserializeObject<ContactsModel>(response.Content);
+            Assert.AreEqual( System.Net.HttpStatusCode.Created, response.StatusCode);
+            JsonContactsModel dataResponse = JsonConvert.DeserializeObject<JsonContactsModel>(response.Content);
             Assert.AreEqual("Clark", dataResponse.First_name);
             Assert.AreEqual("Dsoza", dataResponse.Last_name);
+            Assert.AreEqual("kj road", dataResponse.Address);
+            Assert.AreEqual("NY", dataResponse.City);
+            Assert.AreEqual("bst", dataResponse.State);
+            Assert.AreEqual("123445", dataResponse.Zip);
+            Assert.AreEqual("9999979000", dataResponse.Phone_number);
+            Assert.AreEqual("cdd@gmail.com", dataResponse.Email);
+            Assert.AreEqual("AddressBook1", dataResponse.AddressBookName);
 
         }
     }
